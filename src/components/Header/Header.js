5ae2cc893a604f0e28f957c672aac075afc0128d
Mehmet Tansu Soydan <aidenshaw.leo@gmail.com>
2023-09-06 11:17:58 -0400

    add back button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,26 @@ import { Link } from "react-router-dom";
 import header from "./header.png";
 
 const Header = () => {
+  const hideButton = window.location.href === "http://localhost:3000/";
+
+  const redirectToHomePage = () => {
+    window.location.href = "http://localhost:3000/";
+  };
+
   return (
     <div className="header">
-      <Link to="/">
-        <img className="header-img" src={header} alt="logo" />
-      </Link>
+      <div className="button-container">
+        {!hideButton && (
+          <button className="back-button" onClick={redirectToHomePage}>
+            <span>&#8678;</span>
+          </button>
+        )}
+      </div>
+      <div className="logo-container">
+        <Link to="/">
+          <img className="header-img" src={header} alt="logo" />
+        </Link>
+      </div>
     </div>
   );
 };
