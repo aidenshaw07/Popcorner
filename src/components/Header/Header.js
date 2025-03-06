de37a4ac643a7ea77137944fc5766d94605c86a1
Aiden Shaw <aidenshaw.leo@gmail.com>
2025-03-06 16:47:02 -0500

    update back button styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,13 @@ const Header = () => {
       <div className="button-container">
         {!hideButton && (
           <button className="back-button" onClick={redirectToHomePage}>
-            <span>&#8678;</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="#fff"
+            >
+              <path d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+            </svg>
           </button>
         )}
       </div>
